8b45a13c6bec80de7f254138dc4e80f0b2df27cb
CreaSoley <artcreatifmeg@gmail.com>
2025-11-24 13:05:20 +0100

    Update devinette-du-jour.js

--- js/devinette-du-jour.js
+++ js/devinette-du-jour.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", chargerDevinette);
 function chargerDevinette() {
     const zone = document.querySelector("#devinette-du-jour");
 
-    fetch("https://script.google.com/macros/s/AKfycbzoFRgG1z7veVxYyKIrqTpw9kiFdz_PK9pmo65vSf9qx2OLE2WHR_F-2J1FQJo1jeYDdA/exec")
+    fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhxfxbN0_3pRPksQcLKJiog3wh6_k8VdeifnsBGwnpKQ9knLw5gCXPe4c4DMDteZV9vFvPf4UBacG3YDTRYdKEVc7Rue54juM5LFxUba6vRpOv1xDP2PxoiVu3ynOsUhUvarfO9pOsEH4bgN_du7UuWTczLbD0bofg7uv3mwyv4v_g71OLbU-8IyR9hIpenLSlxXX9aIRYf738jSR2WemZxJbVv9cyRFKMctVZnKZJiCgjIP5cxOE73c-nRawN4R6g4KifnFk3-V6PmJ1YoDNw-ZmI1vIShwEN3ASGh&lib=MuiZkSlYENgyCkMBX3NcyJwTqGYO7_w4Z")
         .then(response => {
             if (!response.ok) throw new Error("Réponse non valide");
             return response.json();
